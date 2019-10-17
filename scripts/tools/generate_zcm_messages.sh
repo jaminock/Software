4a3ba128a67a93f7e4a8b088c7466d38b3c50d25
Jacob Minock <jaminock@umich.edu>
2019-10-16 20:01:57 -0400

    Started work on generate_zcm_messages.sh script to allow compiling of shared library, used for zcm-spy-lite

--- scripts/tools/generate_zcm_messages.sh
+++ scripts/tools/generate_zcm_messages.sh
@@ -5,6 +5,7 @@ SOFTWARE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 SOFTWARE_DIR="$(dirname "$SOFTWARE_DIR")"
 SOFTWARE_DIR="${SOFTWARE_DIR}/.."
 GENERATED_DIR="${SOFTWARE_DIR}/generated/common/messages"
+TMP_GENERATED_DIR="/tmp/maav-zcm-files"
 ZCM_TYPES_DIR="${SOFTWARE_DIR}/msgtypes/zcm"
 
 echo ${SOFTWARE_DIR}
@@ -14,4 +15,18 @@ if [ -d "${SOFTWARE_DIR}/generated/common" ]; then
     rm -r ${SOFTWARE_DIR}/generated/common
 fi
 
+# Delete Temporary stuff
+rm -rf ${TMP_GENERATED_DIR}
+
+# Generate hpp files for using with software development
 zcm-gen --cpp --cpp-hpath ${GENERATED_DIR} ${ZCM_TYPES_DIR}/*.zcm
+
+# Generate shared library to use with zcm-spy-lite
+zcm-gen -c --c-typeinfo --c-hpath ${TMP_GENERATED_DIR} \
+	--c-cpath ${TMP_GENERATED_DIR} ${ZCM_TYPES_DIR}/*.zcm
+echo "zcm-gen ran successfully"
+cd ${TMP_GENERATED_DIR}
+cc -c -fPIC ${TMP_GENERATED_DIR}/*.c
+echo "object files created successfully"
+cc -shared -fPIC -o ${GENERATED_DIR}/zcmlibrary.so *.c
+echo "done"
